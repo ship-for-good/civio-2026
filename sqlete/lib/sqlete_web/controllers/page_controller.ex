892886d8154211da1992e57ba0b5fe942b7bcb7a
Álvaro <alvaro.girona@gmail.com>
2026-05-30 00:04:45 +0200

    feat: add landing

--- sqlete/lib/sqlete_web/controllers/page_controller.ex
+++ sqlete/lib/sqlete_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule SQLeteWeb.PageController do
   use SQLeteWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    render(conn, :home, page_title: "SQLete")
   end
 end
