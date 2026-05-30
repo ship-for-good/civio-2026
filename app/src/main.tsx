cc0821f8d9dd63c28b8d7b2a15e88a6cbcdfaf07
Javier Sánchez <javier.sanchezdaza@MacBook-Pro-de-Javier-Sanchez.local>
2026-05-30 16:07:01 +0200

    - r migrate main.jsx → main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './contexts/AuthContext.jsx'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
